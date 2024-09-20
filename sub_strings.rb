f6ad6b7984bba0423e7d1a2fd43964992d6a1a63
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-09-20 14:47:28 +0200

    Refactor code

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,25 +1,13 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 string = "Howdy partner, sit down! How's it going?"
 
-  def string_normalization(string)
-    string.downcase.gsub(/['’]s|['’]t|['’]ve|['’]re|['’]ll|['’]d|[^\w\s]/, '').split
-  end
-
-  string_normalization(string)
-
-  clean_string = string_normalization(string)
-
-  def substrings(string, dictionary)
-    substring_counts = Hash.new(0)
-    dictionary.each do |dictionary_word|
-      string.each do |string_word|
-        if dictionary_word.include?(string_word)
-          substring_counts[dictionary_word] += 1
-        end
+  def substrings(words, dictionary)
+    dictionary.reduce(Hash.new(0)) do |hash, dictionary_word|
+      words.downcase.split.each do |word|
+        hash[dictionary_word] += 1 if word.include?(dictionary_word)
       end
+      hash
     end
-    substring_counts
   end
 
-  result = substrings(clean_string, dictionary)
-  puts result
+  substrings(string, dictionary)
