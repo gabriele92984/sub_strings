e2be43932600e376c5927a6a83e64df95f61863f
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-09-20 12:47:09 +0200

    Create #substrings method

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,8 +1,8 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+input_dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 input_string = "Howdy partner, sit down! How's it going?"
 
   def string_normalization(text)
-    cleaned_text = text.gsub(/['’]s|['’]t|['’]ve|['’]re|['’]ll|['’]d|[^\w\s]/, '').downcase
+    text.downcase.gsub(/['’]s|['’]t|['’]ve|['’]re|['’]ll|['’]d|[^\w\s]/, '')
   end
 
   string_normalization(input_string)
@@ -10,8 +10,19 @@ input_string = "Howdy partner, sit down! How's it going?"
   clean_string = string_normalization(input_string)
   puts clean_string
 
-  def substrings(word, dictionary)
+  def substrings(string, dictionary)
+    words = string.split
+    substring_counts = Hash.new(0)
 
+    dictionary.each do |dictionary_word|
+      words.each do |string_word|
+        if dictionary_word.include?(string_word)
+          substring_counts[dictionary_word] += 1
+        end
+      end
+    end
+    substring_counts
   end
 
-  substrings(clean_string, dictionary)
+  result = substrings(clean_string, input_dictionary)
+  puts result
