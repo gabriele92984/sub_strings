37eaf9b7548da1ce5ff82e49dcdf28574088e680
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-09-20 15:25:14 +0200

    Restore previous code

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,13 +1,28 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 string = "Howdy partner, sit down! How's it going?"
 
-  def substrings(words, dictionary)
-    dictionary.reduce(Hash.new(0)) do |hash, dictionary_word|
-      words.downcase.split.each do |word|
-        hash[dictionary_word] += 1 if word.include?(dictionary_word)
+def string_normalization(str)
+  str.downcase.gsub(/['’]s|['’]t|['’]ve|['’]re|['’]ll|['’]d|[^\w\s]/, '')
+end
+
+# Normalize the string once and store it in normalized_string
+normalized_string = string_normalization(string)
+
+def substrings(words, dictionary)
+  substring_counts = Hash.new(0)
+
+  # Iterate over each word in the normalized_string
+  words.split.each do |word|
+    # Check if the current word exists in the dictionary
+    dictionary.each do |dictionary_word|
+      if word == dictionary_word
+        substring_counts[dictionary_word] += 1
       end
-      hash
     end
   end
 
-  substrings(string, dictionary)
+  substring_counts
+end
+
+result = substrings(normalized_string, dictionary)
+puts result
