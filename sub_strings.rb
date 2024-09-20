c015caaf296d247b14b88eeadb3ef0838d5fd4ba
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-09-20 14:20:42 +0200

    Clean up code

--- sub_strings.rb
+++ sub_strings.rb
@@ -2,20 +2,17 @@ input_dictionary = ["below","down","go","going","horn","how","howdy","it","i","l
 input_string = "Howdy partner, sit down! How's it going?"
 
   def string_normalization(text)
-    text.downcase.gsub(/['’]s|['’]t|['’]ve|['’]re|['’]ll|['’]d|[^\w\s]/, '')
+    text.downcase.gsub(/['’]s|['’]t|['’]ve|['’]re|['’]ll|['’]d|[^\w\s]/, '').split
   end
 
   string_normalization(input_string)
 
   clean_string = string_normalization(input_string)
-  puts clean_string
 
   def substrings(string, dictionary)
-    words = string.split
     substring_counts = Hash.new(0)
-
     dictionary.each do |dictionary_word|
-      words.each do |string_word|
+      string.each do |string_word|
         if dictionary_word.include?(string_word)
           substring_counts[dictionary_word] += 1
         end
